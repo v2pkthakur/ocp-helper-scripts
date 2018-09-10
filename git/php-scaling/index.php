d922d1da1ec1c30c8d8d5cf8f4bcc9ceba2271fc
root <root@base.example.com>
2018-09-10 17:38:54 +0800

    Added Sample Git PHP Repo

---
+++ git/php-scaling/index.php
@@ -0,0 +1,12 @@
+<html>
+<title> Welcome to Sample PHP Scaling App.</title>
+<body>
+<br/>
+<br/>
+<p align=center> Welcome to Apache/PHP App. </p>
+<br/>
+
+</br> Server IP: <?php echo $_SERVER['SERVER_ADDR'] ?>
+
+</body>
+</html>

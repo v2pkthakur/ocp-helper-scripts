590286e84937fc7f79d58cfd9acd96855641f6ee
root <root@bastion.033b.example.opentlc.com>
2018-10-11 08:22:59 +0000

    cleanup and added setup-hosts.sh

--- setup-hosts.sh
+++ setup-hosts.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ansible-playbook pre-reqs.yaml --ask-vault-pass
+ansible-playbook -v pre-reqs.yaml --ask-vault-pass
+cp tested-copy-28082018/hosts /etc/ansible/hosts
+mkdir /etc/ansible/group_vars
+cp tested-copy-28082018/OSEv3.yaml /etc/ansible/group_vars/
+

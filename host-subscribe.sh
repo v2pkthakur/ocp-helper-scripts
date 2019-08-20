cac0fdc1c48790f0b881a083b99f4be343aff2ac
Praveen Thakur <v2pkthakur@gmail.com>
2019-08-20 13:41:03 +0800

    Fix hosts-subscribe

--- host-subscribe.sh
+++ host-subscribe.sh
@@ -3,6 +3,8 @@
 subscription-manager register --username prthakur@redhat.com
 subscription-manager attach --pool=8a85f99b65c8c8f1016651638fe56010
 subscription-manager repos --disable='*'
-subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-optional-rpms --enable=rhel-7-server-ose-3.11-rpms --enable=rhel-7-server-rh-common-rpms
-subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-optional-rpms --enable=rhel-7-server-ose-3.11-rpms --enable=rhel-7-server-rh-common-rpms --enable=--enable=rh-gluster-3-for-rhel-7-server-rpms 
+subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-ose-3.11-rpms --enable=rhel-7-server-ansible-2.6-rpms
+
+
+#subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-optional-rpms --enable=rhel-7-server-ose-3.11-rpms --enable=rhel-7-server-rh-common-rpms --enable=--enable=rh-gluster-3-for-rhel-7-server-rpms 
 
